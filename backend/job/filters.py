b14926c850dd90fcf0e18c375c20e73128182091
Negar Nadian <nnadian79@gmail.com>
2023-03-26 01:34:38 +0430

    add search job by title to filters

--- backend/job/filters.py
+++ backend/job/filters.py
@@ -3,8 +3,9 @@ from .models import Job
 
 
 class JobsFilter(filters.FilterSet):
+    keyword = filters.CharFilter(field_name='title', lookup_expr='icontains')
     min_salary = filters.NumberFilter(field_name="salary" or 0, lookup_expr='gte')
     max_salary = filters.NumberFilter(field_name="salary" or 100000000, lookup_expr='lte')
     class Meta:
         model = Job
-        fields = ('education', 'jobType', 'experience', 'min_salary', 'max_salary')
+        fields = ('keyword', 'education', 'jobType', 'experience', 'min_salary', 'max_salary')
