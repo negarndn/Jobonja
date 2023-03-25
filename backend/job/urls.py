ece7737e0cafb701c917aae0371638a52a0bd7cb
Negar Nadian <nnadian79@gmail.com>
2023-03-25 20:07:48 +0430

    add create new job and update job

--- backend/job/urls.py
+++ backend/job/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('jobs/', views.getAllJobs, name='jobs'),
     path('jobs/<str:pk>', views.getJob, name='job'),
+    path('jobs/new/', views.newJob, name='new_job'),
+    path('jobs/<str:pk>/update/', views.updateJob, name='update_job'),
+
 ]
--- backend/job/views.py
+++ backend/job/views.py
@@ -19,3 +19,33 @@ def getJob(request, pk):
     job = get_object_or_404(Job, id=pk)
     serializer = JobSerializer(job, many=False)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def newJob(request):
+    data = request.data
+    job = Job.objects.create(**data)
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def updateJob(request, pk):
+    job = get_object_or_404(Job, id=pk)
+
+    job.title = request.data['title']
+    job.description = request.data['description']
+    job.email = request.data['email']
+    job.address = request.data['address']
+    job.jobType = request.data['jobType']
+    job.education = request.data['education']
+    job.industry = request.data['industry']
+    job.experience = request.data['experience']
+    job.salary = request.data['salary']
+    job.positions = request.data['positions']
+    job.company = request.data['company']
+
+    job.save()
+
+    serializer = JobSerializer(job, many=False)
+    return Response(serializer.data)
