71f40a3ddcd85bce52902f2e0eac62c6fc5a235a
Negar Nadian <nnadian79@gmail.com>
2023-03-27 16:39:09 +0430

    change resPerPage value

--- backend/job/views.py
+++ backend/job/views.py
@@ -16,7 +16,7 @@ def getAllJobs(request):
     filterset = JobsFilter(request.GET, queryset=Job.objects.all().order_by('id'))
     count = filterset.qs.count()
     #pagination
-    resPerPage = 3
+    resPerPage = 10
     paginator = PageNumberPagination()
     paginator.page_size = resPerPage
     queryset = paginator.paginate_queryset(filterset.qs, request)
