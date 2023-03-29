a23abcc8b97f3acc854f4d7bcf055dfbc4cfc023
Nafis Momeni <77436663+nafis-momeni@users.noreply.github.com>
2023-03-29 16:57:19 +0430

    create register view and url

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth.hashers import make_password
+from .serializers import SignUpSerializer, UserSerializer
+
+from rest_framework.permissions import IsAuthenticated
+
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+
+    user = SignUpSerializer(data=data)
+
+    if user.is_valid():
+        if not User.objects.filter(username=data['email']).exists():
+            user = User.objects.create(
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                username=data['email'],
+                email=data['email'],
+                password=make_password(data['password'])
+            )
+
+            return Response({
+                'message': 'User registered.'},
+                status=status.HTTP_200_OK
+            )
+        else:
+            return Response({
+                'error': 'User already exists'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    else:
+        return Response(user.errors)
 
 # Create your views here.
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('job.urls'))
+    path('api/', include('job.urls')),
+    path('api/', include('account.urls'))
 ]
