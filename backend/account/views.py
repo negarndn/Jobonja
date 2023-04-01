f5eaa10103f9621d0bf56191ec4e87594f9af3ec
Nafis Momeni <77436663+nafis-momeni@users.noreply.github.com>
2023-04-01 18:56:19 +0430

    create update user profile view

--- backend/account/views.py
+++ backend/account/views.py
@@ -50,6 +50,26 @@ def currentUser(request):
 
     return Response(user.data)
 
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request):
+    user = request.user
+
+
+    data = request.data
+
+    user.first_name = data['first_name']
+    user.last_name = data['last_name']
+    user.username = data['email']
+    user.email = data['email']
+
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 
 
