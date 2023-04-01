4351f140869133a93347c9f9e6155b69eb1db943
Nafis Momeni <77436663+nafis-momeni@users.noreply.github.com>
2023-04-01 16:28:40 +0430

    login

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('job.urls')),
-    path('api/', include('account.urls'))
+    path('api/', include('account.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/verify/', TokenVerifyView.as_view())
 ]
