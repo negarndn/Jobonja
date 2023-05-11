7a56be0b66b6c0e13b8b4bcce8f8f8c058c33e0c
parisa <prisashahbi@gmail.com>
2023-05-11 19:46:09 +0330

    setup jest

---
+++ frontend/pages/home.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </div>
+  );
+}
