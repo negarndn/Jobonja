5b0c1165c2b0290d330190d1d33f528eba03e127
parisa <prisashahbi@gmail.com>
2023-05-14 10:52:10 +0330

    refactor eArabic unit test

--- frontend/__tests__/eArabic.test.js
+++ frontend/__tests__/eArabic.test.js
@@ -1,21 +1,66 @@
 import eArabic from "../utils/eArabic";
 
 describe("eArabic", () => {
-  it("should return the Arabic locale string for a given number", () => {
-    expect(eArabic(123)).toEqual("١٢٣");
-    expect(eArabic(123456.789)).toEqual("١٢٣٬٤٥٦٫٧٨٩");
-    expect(eArabic(0)).toEqual("٠");
-    // expect(eArabic(-9)).toEqual("-٩");
+  test("should return a number in Arabic format", () => {
+    // Arrange
+    const number = 12345;
+
+    // Act
+    const result = eArabic(number);
+
+    // Assert
+    expect(result).toBe("١٢٬٣٤٥");
   });
 
-  it("should return the Arabic locale string for a given number even if it is in string format", () => {
-    expect(eArabic("123")).toEqual("١٢٣");
+  test("should return Arabic formatted string for 0", () => {
+    expect(eArabic(0)).toBe("٠");
   });
 
-  it("should return the original input if it is null, undefined, or string", () => {
-    expect(eArabic("Some invalid input")).toBe("Some invalid input");
-    expect(eArabic(null)).toBe(null);
-    expect(eArabic(NaN)).toBe(NaN);
-    expect(eArabic(undefined)).toBe(undefined);
+  // test("should return Arabic formatted string for negative number", () => {
+  //   expect(eArabic(-123)).toBe("-١٢٣");
+  // });
+
+  test("should return Arabic formatted string for floating point number", () => {
+    expect(eArabic(123.45)).toBe("١٢٣٫٤٥");
+  });
+
+  test("should return Arabic formatted string for number in form of string", () => {
+    expect(eArabic("123")).toBe("١٢٣");
+  });
+
+  test("should return a string without any changes", () => {
+    // Arrange
+    const string = "Hello World";
+
+    // Act
+    const result = eArabic(string);
+
+    // Assert
+    expect(result).toBe(string);
+  });
+
+  test("should return the same input for null or undefined input", () => {
+    // Arrange
+    const nullInput = null;
+    const undefinedInput = undefined;
+
+    // Act
+    const nullResult = eArabic(nullInput);
+    const undefinedResult = eArabic(undefinedInput);
+
+    // Assert
+    expect(nullResult).toBe(nullInput);
+    expect(undefinedResult).toBe(undefinedInput);
+  });
+
+  test("should return the same input for NaN input", () => {
+    // Arrange
+    const nanInput = NaN;
+
+    // Act
+    const result = eArabic(nanInput);
+
+    // Assert
+    expect(result).toBe(nanInput);
   });
 });
