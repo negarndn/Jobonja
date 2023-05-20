7a4b2cf092e0b609bccff4290ba8334b98211078
parisa <prisashahbi@gmail.com>
2023-05-20 22:59:11 +0330

    refactor tests

--- frontend/__tests__/encodeFilters.test.js
+++ frontend/__tests__/encodeFilters.test.js
@@ -2,87 +2,189 @@ import encodeFilters from "../utils/encodeFilters";
 
 describe("encodeFilters function", () => {
   test("should encode 'Bachelors' filter correctly", () => {
-    const result = encodeFilters("Bachelors");
+    // Arrange
+    const filter = "Bachelors";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("کارشناسی");
   });
 
   test("should encode 'Masters' filter correctly", () => {
-    const result = encodeFilters("Masters");
+    // Arrange
+    const filter = "Masters";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("کارشناسی ارشد");
   });
 
   test("should encode 'Phd' filter correctly", () => {
-    const result = encodeFilters("Phd");
+    // Arrange
+    const filter = "Phd";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("دکتری");
   });
 
   test("should encode 'Permanent' filter correctly", () => {
-    const result = encodeFilters("Permanent");
+    // Arrange
+    const filter = "Permanent";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("تمام‌وقت");
   });
 
   test("should encode 'Temporary' filter correctly", () => {
-    const result = encodeFilters("Temporary");
+    // Arrange
+    const filter = "Temporary";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("پاره‌وقت");
   });
 
   test("should encode 'Internship' filter correctly", () => {
-    const result = encodeFilters("Internship");
+    // Arrange
+    const filter = "Internship";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("کارآموزی");
   });
 
   test("should encode 'No Experience' filter correctly", () => {
-    const result = encodeFilters("No Experience");
+    // Arrange
+    const filter = "No Experience";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("بدون محدودیت سابقه کار");
   });
 
   test("should encode '1 Year' filter correctly", () => {
-    const result = encodeFilters("1 Year");
+    // Arrange
+    const filter = "1 Year";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("حداقل یک سال");
   });
 
   test("should encode '2 Years' filter correctly", () => {
-    const result = encodeFilters("2 Years");
+    // Arrange
+    const filter = "2 Years";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("حداقل دو سال");
   });
 
   test("should encode '3 Years Above' filter correctly", () => {
-    const result = encodeFilters("3 Years Above");
+    // Arrange
+    const filter = "3 Years Above";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("بیش از سه سال");
   });
 
   test("should encode 'Business' filter correctly", () => {
-    const result = encodeFilters("Business");
+    // Arrange
+    const filter = "Business";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("فروش و بازایابی");
   });
 
   test("should encode 'IT' filter correctly", () => {
-    const result = encodeFilters("IT");
+    // Arrange
+    const filter = "IT";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("وب، برنامه‌نویسی و نرم‌افزار");
   });
 
   test("should encode 'Banking' filter correctly", () => {
-    const result = encodeFilters("Banking");
+    // Arrange
+    const filter = "Banking";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("مالی و حسابداری");
   });
 
   test("should encode 'Education' filter correctly", () => {
-    const result = encodeFilters("Education");
+    // Arrange
+    const filter = "Education";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("کارشناسی");
   });
 
   test("should encode 'Telecommunication' filter correctly", () => {
-    const result = encodeFilters("Telecommunication");
+    // Arrange
+    const filter = "Telecommunication";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("روابط عمومی");
   });
 
   test("should encode 'Others' filter correctly", () => {
-    const result = encodeFilters("Others");
+    // Arrange
+    const filter = "Others";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("متفرقه");
   });
 
   test("should return empty string when given an unknown filter", () => {
-    const result = encodeFilters("Unknown");
+    // Arrange
+    const filter = "Unknown";
+
+    // Act
+    const result = encodeFilters(filter);
+
+    // Assert
     expect(result).toEqual("");
   });
 });
