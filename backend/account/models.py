a83cd77b85283e72dbce0ce2ccb10770b0aee7a8
Nafis Momeni <77436663+nafis-momeni@users.noreply.github.com>
2023-06-19 22:49:25 +0430

    add signals to save new user and send welcoming email

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.dispatch import receiver
-from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
@@ -9,11 +7,4 @@ class UserProfile(models.Model):
     resume = models.FileField(null=True)
 
 
-@receiver(post_save, sender=User)
-def save_profile(sender, instance, created, **kwargs):
-    # print(instance)
-    user = instance
 
-    if created:
-        profile = UserProfile(user=user)
-        profile.save()
